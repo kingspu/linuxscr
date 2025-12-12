4ce8e208cdf98903f4f39570ebf5ed2a38be0ec4
kingspu <danny242009@yahoo.com>
2025-12-11 20:52:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def alterConfig(dir, oldConfig,newConfig):
-    pass
 
 def alterConfigs(dirs, oldConfigs,newConfigs):
     """First arg: list of file directories you wish to alter
@@ -49,8 +47,9 @@ def alterConfigs(dirs, oldConfigs,newConfigs):
             f.writelines(editedConfigs)
         i+=1
 
-t = "TRUE"
-f = "FALSE"
-alterConfigs([r"C:\Users\danny.DESKTOP-GRB5QI1\OneDrive\Desktop\donfig.txt"],[["config1:","config2:"]],[["config1: 41","config2: 67"]])
-#alterConfigs(["test.txt"],[["this is another config:", "this is a config:", "this is another different config:"]],[[f"this is another config: {f}",f"this is a config: {t}",f"this is another different config: {f}"]])
-#alterConfigs(["test.txt","hello.txt"],["this is another another config:","this is a config"],["this is another another config: FALSE","this is a config: FALSE"])
+#ssh configs
+#alterConfigs(["/etc/ssh/sshd_config"],[["LoginGraceTime","PermitRootLogin","Protocol","PermitEmptyPasswords","PasswordAuthentication","X11Forwarding","UsePAM","UsePrivilegeSeparation"]],[["LoginGraceTime 60","PermitRootLogin no","Protocol 2","PermitEmptyPasswords no","PasswordAuthentication yes","X11Forwarding no","UsePAM yes","UsePrivilegeSeparation yes"]])
+#sysctl configs
+#alterConfigs(["/etc/sysctl.conf"],[["net.ipv4.conf.all.accept_redirects","net.ipv4.ip_forward","net.ipv4.conf.all.send_redirects","net.ipv4.conf.default.send_redirects","net.ipv4.conf.all.rp_filter","net.ipv4.conf.all.accept_source_route","net.ipv4.tcp_max_syn_backlog","net.ipv4.tcp_synack_retries","net.ipv4.tcp_syn_retries","net.ipv4.tcp_syncookies","net.ipv6.conf.all.disable_ipv6","net.ipv6.conf.default.disable_ipv6","net.ipv6.conf.lo.disable_ipv6"]],[["net.ipv4.conf.all.accept_redirects = 0","net.ipv4.ip_forward = 0","net.ipv4.conf.all.send_redirects = 0","net.ipv4.conf.default.send_redirects = 0","net.ipv4.conf.all.rp_filter=1","net.ipv4.conf.all.accept_source_route=0","net.ipv4.tcp_max_syn_backlog = 2048","net.ipv4.tcp_synack_retries = 2","net.ipv4.tcp_syn_retries = 5","net.ipv4.tcp_syncookies = 1","net.ipv6.conf.all.disable_ipv6 = 1","net.ipv6.conf.default.disable_ipv6 = 1","net.ipv6.conf.lo.disable_ipv6 = 1"]])
+#pam configs
+#alterConfigs(["/etc/login.defs"],[],[["FAILLOG_ENAB yes","LOG_UNKFAIL_ENAB yes","SYSLOG_SU_ENAB yes","SYSLOG_SG_ENAB yes","PASS_MAX_DAYS 90","PASS_MIN_DAYS 10","PASS_WARN_AGE 7"]])
